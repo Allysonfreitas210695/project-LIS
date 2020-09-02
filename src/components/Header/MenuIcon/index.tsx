1d322e9270232fd3dde3e54e1da9888f352ecca5
Allysonfreitas210695 <alisonfr83@gmail.com>
2020-09-02 09:46:50 -0300

    add media query responsividade

--- src/components/Header/MenuIcon/index.tsx
+++ src/components/Header/MenuIcon/index.tsx
@@ -4,6 +4,10 @@ import styled from 'styled-components';
 const MenuIcon = styled.div`
     font-size: 2rem;
     color: var(--color-white);
+
+    @media(min-width:769px){
+        display:none;
+    }
 `;
 
 export default MenuIcon;
